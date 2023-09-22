3ad59883c10b98fcea4528f197bca03e8d6efe60
Pablo Bustos <pablobustos609@gmail.com>
2023-09-21 22:30:51 -0300

    add edit and delete handlers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, NotFoundException, Param, Post, Put } from '@nestjs/common';
 import { data, Report, ReportType } from './data';
 import { v4 } from "uuid"
 
@@ -39,16 +39,29 @@ export class AppController {
   }
 
   @Put(':id')
-  updateReport(@Param('id') id: string) {
-    console.log('resport', id);
+  updateReport(@Body() body: {amount: number, source: string}, @Param("type") type: ReportType, @Param('id') id: string) {
+    const { source, amount } = body
 
-    return {};
+    const report = data.report.find(report => report.id === id && report.type === type)
+    
+    if(!report) throw new NotFoundException();
+
+    if(source) report.source = source
+    if(amount) report.amount = amount
+    if(amount || source) report.updated_at = new Date()
+
+    return report;
   }
 
   @Delete(':id')
-  deleteReport(@Param('id') id: string) {
-    console.log('resport', id);
+  @HttpCode(HttpStatus.NO_CONTENT)
+  deleteReport(@Param("type") type: ReportType, @Param('id') id: string) {
+    const deletedReport = data.report.find(report => report.id === id && report.type === type)
 
-    return {};
+    if(!deletedReport) throw new NotFoundException();
+
+    data.report = data.report.filter(report => report.id !== id)
+
+    return
   }
 }
