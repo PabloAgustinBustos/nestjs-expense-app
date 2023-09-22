aea304b43adbc8d59df4326b0f0c2bc24b995793
Pablo Bustos <pablobustos609@gmail.com>
2023-09-21 22:01:53 -0300

    add createReport handler

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,19 @@
-import { Controller, Delete, Get, NotFoundException, Param, Post, Put } from '@nestjs/common';
-import { data, ReportType } from './data';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Put } from '@nestjs/common';
+import { data, Report, ReportType } from './data';
+import { v4 } from "uuid"
 
 @Controller('report/:type')
 export class AppController {
   @Get()
-  getAllIncomeReports(@Param("type") type: string) {
-    const reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE
-
-    const reports = data.report.filter(report => report.type === reportType)
+  getAllIncomeReports(@Param("type") type: ReportType) {
+    const reports = data.report.filter(report => report.type === type)
    
     return reports;
   }
 
   @Get(':id')
-  getReport(@Param('id') id: string, @Param("type") type: string) {
-    const reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE
-
-    const report = data.report.find(report => report.type === reportType && report.id === id)
+  getReport(@Param('id') id: string, @Param("type") type: ReportType) {
+    const report = data.report.find(report => report.type === type && report.id === id)
 
     if(!report) throw new NotFoundException();
 
@@ -24,8 +21,21 @@ export class AppController {
   }
 
   @Post()
-  createReport() {
-    return {};
+  createReport(@Body() body: {amount: number, source: string}, @Param("type") type: ReportType) {
+    const { source, amount } = body
+    
+    const newReport: Report = {
+      id: v4(),
+      source,
+      amount,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type
+    }
+
+    data.report.push(newReport)
+
+    return newReport;
   }
 
   @Put(':id')
